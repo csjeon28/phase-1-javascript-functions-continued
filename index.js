6efee03a6fe5b82b194140b08298f358b40cf517
Christine Jeon <csjeon28@gmail.com>
2021-04-08 02:44:17 -0400

    commit tree 8055e0a13b88fe31509260d3689eeed2b1e13e06

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog");
+
+const mondayWork = (function mondayWork(activity="go to the office") {
+    return (`This Monday, I will ${activity}.`);
+})
+mondayWork("work from home");
+
+function wrapAdjective(wrapper) {
+    return function(msg) {
+        if (wrapper == null) {
+        return `You are *${msg}*!`
+        } else {
+            return `You are ${wrapper}${msg}${wrapper}!`
+        }
+    }
+}
+
+// function wrapAdjective() {
+//     return function(awesome) {
+//         return `You are *${awesome}*!`
+//     }
+// }
